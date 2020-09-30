c10eba23bba94bf3b95c207794601a4adfb41b11
anaeli24 <70556194+anaeli24@users.noreply.github.com>
2020-09-30 10:54:04 -0500

    cambiost2

--- t2/tarea2.py
+++ t2/tarea2.py
@@ -1,10 +1,12 @@
+#Shcaeffer,E cambio en el codigo
+#Solis,D., Quiñones, O, en apoyo para modificaciones en el codigo
 import numpy as np 
 from random import uniform,random 
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
 
-dim = 20
+dim = 20               #definir 20x20
 num = dim**2
 #for prob in [0.1-0.9]:
 prob = uniform(0.1,0.9)
@@ -40,7 +42,7 @@ if __name__ == "__main__":
     lista_posx=[]             #vacia
     lista_posy=[]
     listaiter=[]
-    salida=open('vivos.txt','w')
+    salida=open('vivos.txt','w')        #para crear doc de texto con las posiciones
     for iteracion in range(50): #num de repeticiones
         print("Iter", iteracion)
         valores = [paso(x) for x in range(num)]   #regla de la vida
@@ -58,18 +60,18 @@ if __name__ == "__main__":
             for m in range (dim):
                 if mvivos[n,m]==True:
                    print(n,m,iteracion)
-                   lista_posx.append(n)
+                   lista_posx.append(n)                     # para poder llenar el text
                    lista_posy.append(m)
                    listaiter.append(iteracion)
                    salida.write("%f %f %f \n" % (n,m,iteracion))           
         fig = plt.figure()
-        plt.imshow(actual, interpolation='nearest', cmap=cm.Greys)
+        plt.imshow(actual, interpolation='nearest', cmap=cm.Greys)     #para las imagenes del gif
         fig.suptitle('Paso {:d}'.format(iteracion + 1))
         plt.savefig('p2_t{:d}_p.png'.format(iteracion + 1))  
         plt.close()
     salida.close()
-    plt.plot(lista_posx,lista_posy,'o')
-    plt.xlabel('x')
+    plt.plot(lista_posx,lista_posy,'o')           #graficar las posiciones mapeadas
+    plt.xlabel('x')                               #manual mathplotlib
     plt.ylabel('y')
     for i,txt in enumerate (listaiter):
         plt.annotate(str(txt),(lista_posx[i],lista_posy[i]))
